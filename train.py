e0bdef6a4e8d30684246b83a6d4655df29d30a9a
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2018-09-09 01:06:11 +0900

    add argparse

--- train.py
+++ train.py
@@ -76,5 +76,86 @@ class Trainer:
                                         feed_dict = {self.images: images, self.t: t})
 
                 if i%20 == 0:
-                    
-        
+                    batch_time = time.time() - time_s
+                    kwargs = {'loss':loss, 'batch time'batch_time}
+                    show_progress(e+1, i+1, self.num_batches, **kwargs)
+
+            loss_vals = []
+            for images_val, t_val in self.vloader:
+                images_val = images_val.numpy()/255.0
+                t_val = t_val.numpy()
+
+                flow_val, loss_val \
+                    = self.sess.run([self.flow, self.loss],
+                                    feed_dict = {self.images: images_val, self.t: t_val})
+                loss_vals.append(loss_val)
+
+            print(f'\r{e+1} epoch validation, loss: {np.mean(loss_vals)}'\
+                  +f', elapsed time: {time.time()-train_start} sec.')
+
+            # TODO: visualize estimated results
+            if self.args.visualize:
+                if not os.path.exists('./figure'):
+                    os.mkdir('./figure')
+                
+
+            if not os.path.exists('./model'):
+                os.mkdir('./model')
+            self.saver.save(self.sess, f'./model/model_{e+1}.ckpt')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type = str, required = True,
+                        help = 'Target dataset, required')
+    parser.add_argument('--dataset_dir', type = str, required = True,
+                        help = 'Directory containing target dataset, required')
+    parser.add_argument('--strides', type = int, default = 3,
+                        help = 'Frame interval of training data [3]')
+    parser.add_argument('--stretchable', action = 'store_true',
+                        help = 'Stored option for frame sampling')
+    
+    parser.add_argument('--resolution', type = str, default = '480p',
+                        choices = ['480p', 'Full-resolution'],
+                        help = 'Resolution option for DAVIS dataset')
+    parser.add_argument('--mode', type = str, default = 'clean',
+                        choices = ['clean', 'final']
+                        help = 'Image quality option for MPI-Sintel dataset')
+    
+    parser.add_argument('--num_epochs', type = int, default = 100,
+                        help = '# of epochs [100]')
+    parser.add_argument('--batch_size', type = int, default = 16,
+                        help = 'Batch size [16]')
+    parser.add_argument('--num_workers', type = int, default = 8,
+                        help = '# of workers for data loading [8]')
+
+    parser.add_argument('--crop_type', type = str, default = 'random',
+                        help = 'Crop type for raw images [random]')
+    parser.add_argument('--crop_shape', nargs = 2, type = int, default = [256, 320],
+                        help = 'Crop shape for raw images [256, 320]')
+    parser.add_argument('--resize_shape', nargs = 2, type = int, default = None,
+                        help = 'Resize shape for raw images [None]')
+    parser.add_argument('--resize_scale', type = float, default = None,
+                        help = 'Resize scale for raw images [None]')
+    parser.add_argument('--image_size', nargs = 2, type = int, default = [256, 320],
+                        help = 'Image size to be processed [256, 320]')
+
+    parser.add_argument('--lr', type = float, default = 1e-4,
+                        help = 'Learning rate [1e-4]')
+
+    parser.add_argument('-v', '--visualize', dest = 'visualize', action = 'store_true',
+                        help = 'Enable output visualization, [enabled]')
+    parser.add_argument('--no_visualize', dest = 'visualize', action = 'store_false',
+                        help = 'Disable output visualization, [enabled]')
+    parser.add_argument(visualize = True)
+    parser.add_argument('--resume', type = str, default = None,
+                        help = 'Learned parameter checkpoint file [None]')
+
+    args = parser.parse_args()
+    for key, value in vars(args).items():
+        print(f'{key} : {value}')
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = input('Input utilize gpu-id (-1:cpu) : ')
+
+    trainer = Trainer(args)
+    trainer.train()
