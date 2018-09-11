05cbecd6ea263f80843bef27cf68ce2c8bb02821
Daigo Hirooka <daigo@pluto.murata.eb.waseda.ac.jp>
2018-09-11 20:58:19 +0900

    change to return mean value

--- losses.py
+++ losses.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 
 def L1loss(images_gt, images_pred):
-    return tf.reduce_mean(tf.reduce_sum(tf.abs(images_gt-images_pred), axis = (1,2,3)))
+    return tf.reduce_mean(tf.reduce_sum(tf.abs(images_gt-images_pred), axis = -1))
 
